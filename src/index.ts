928bc27b0875e4f8355e9d8c0aa01fbad5d7f743
tatobig <tatobig@gmail.com>
2022-11-10 01:50:18 -0600

    Change port

--- src/index.ts
+++ src/index.ts
@@ -40,7 +40,7 @@ app.use((req, res, next) => {
 });
 
 // #6
-app.listen(8000, () =>
+app.listen(process.env.PORT, () =>
   console.log("Servido escuchando en el la dirección: http://localhost:" + 8000)
 );
 
